3f73af7ef928bf88a87f03f1074d2a4335e9fa53
OswaldoLove253 <48838607+OswaldoStm1@users.noreply.github.com>
2026-03-16 13:41:28 -0600

    Enhance Mazo class with special cards and duplicates
    Added special cards and duplicate numbers to the deck.

--- Mazo.java
+++ Mazo.java
@@ -8,11 +8,29 @@ public class Mazo {
     }
     private void inicializar() {
         String[] colores = {"Rojo", "Azul", "Verde", "Amarillo"};
+        //llenar numeros
         for (String color : colores) {
             for (int i = 0; i <= 9; i++) {
                 cartas.push(new Carta(color, i));
+                if(i!=0){
+                    cartas.push(new Carta(color, i));
+                }
             }
+            cartas.add(new Carta(color,Carta.Tipo.SALTO));
+            cartas.add(new Carta(color,Carta.Tipo.SALTO));
+            cartas.add(new Carta(color,Carta.Tipo.REVERSA));
+            cartas.add(new Carta(color,Carta.Tipo.REVERSA));
+            cartas.add(new Carta(color,Carta.Tipo.ROBA2));
+            cartas.add(new Carta(color,Carta.Tipo.ROBA2));
         }
+        //llenar cartas especiales
+        for(int i = 0;i < 4; ){
+            cartas.add(new Carta("negra",Carta.Tipo.COMODIN));
+            cartas.add(new Carta("negro",Carta.Tipo.ROBA4));
+        }
+
+        //System.out.println(cartas.size());
+
     }
     private void barajar() {
         Collections.shuffle(cartas);
